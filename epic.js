30abec663b6dd92682cd11a0f93d9bc057d33556
tee-o28 <tee.o2809@gmail.com>
2023-02-14 22:01:06 +0100

    New Updates

--- epic.js
+++ epic.js
@@ -2,21 +2,40 @@ let qs = (e) => document.querySelector(e);
 let qsa = (e) => document.querySelectorAll(e);
 
 document.addEventListener('DOMContentLoaded', () => {
-    let h_calcs = document.querySelectorAll('.h-calc');
+    // Const variables
+    const h_calcs = document.querySelectorAll('.h-calc');
 
-    h_calcs.forEach(e => {
-        if (e.clientWidth > 768) {
-            let tall = e.offsetHeight
-            e.style.height = tall + 'px' 
-        }
-    });
+    const epic_demo_div = qs("#epic_demo_div");
+    const epic_demo_div_classlist = epic_demo_div.classList;
+    const epic_applied_classes = qs("#epic_applied_classes");
+    let epic_toggle_class_title = () => qsa(".epic_toggle_class_title");
 
     const menuBtn = qs(".epic_menu-btn");
     const closeMenuBtn = qs(".epic_close-nav");
     const copyBtns = qsa(".epic_copy-btns");
 
+    const observe_anim = qsa('.observe_anim');
+
+    // Event Listeners
     menuBtn.addEventListener("click", dropmenu);
     closeMenuBtn.addEventListener("click", dropmenu);
+
+    // For Each Functions
+    h_calcs.forEach(e => {
+        if (e.clientWidth > 768) {
+            let tall = e.offsetHeight;
+            e.style.height = tall + 'px' 
+        }
+    });
+
+    epic_toggle_class_title().forEach( (btns) => {
+        btns.addEventListener('click', (e) => {
+            epic_demo_div.classList.toggle(e.target.title);
+            getClassList();
+            e.target.classList.toggle('primary-alt');
+            e.target.classList.toggle('bg-grey');
+        });
+    });
 
     copyBtns.forEach( (btns) => {
         btns.addEventListener('click', (e) => {
@@ -25,10 +44,45 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    // Custom Functions
     function dropmenu(e) {
         qs('.epic_nav').classList.toggle('active')
         qs('.epic_nav').classList.toggle('up-in')
     }
+
+    function getClassList() {
+        epic_applied_classes.innerHTML = '';
+        epic_demo_div_classlist.forEach(e => {
+            let btn = document.createElement('span');
+            btn.classList = "btn mr-1 mb-1 bdrs-pill epic_toggle_class_title";
+            btn.innerText = e;
+            btn.setAttribute('title', e)
+            epic_applied_classes.append(btn);
+        });
+    }
+
+    getClassList()
+
+
+    // Js inbuilt Functions
+    observer = new IntersectionObserver(entries => {
+        entries.forEach( entry => {
+            if (entry.target.classList.contains('observe_once') && entry.isIntersecting) observer.unobserver(entry);
+            if (entry.target.classList.contains('anim_left')) entry.target.classList.toggle("left-in", entry.isIntersecting);
+            if (entry.target.classList.contains('anim_right')) entry.target.classList.toggle("right-in", entry.isIntersecting);
+            if (entry.target.classList.contains('anim_up')) entry.target.classList.toggle("up-in", entry.isIntersecting);
+            if (entry.target.classList.contains('anim_down')) entry.target.classList.toggle("down-in", entry.isIntersecting);
+            if (entry.target.classList.contains('anim_roll')) entry.target.classList.toggle("roll-in", entry.isIntersecting);
+            else entry.target.classList.toggle("right-in", entry.isIntersecting);
+        }), 
+        {
+            rootMargin: "20px"
+        }
+    });
+
+    observe_anim.forEach(card => {
+        observer.observe(card);
+    })
 })
 
 let links = [
@@ -55,4 +109,3 @@ for (let i = 0; i < links.length; i++) {
     
     document.querySelector('head').append(linked);    
 }
-
